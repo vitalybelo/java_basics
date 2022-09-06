ee49ff8a1bc189f1cc296106fcb5d26ede366564
vitaly beloserkovskii <vitaly.belo@gmail.com>
2022-09-06 14:53:46 +0300

    finalized

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -1,17 +1,42 @@
+import com.skillbox.airport.Aircraft;
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-import java.util.Collections;
-import java.util.List;
+import com.skillbox.airport.Terminal;
+
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
 
+        findPlanesLeavingInTheNextTwoHours(Airport.getInstance())
+                .forEach(System.out::println);
 
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+        //TODO Метод должен вернуть список рейсов вылетающих в ближайшие два часа.
+        List<Terminal> terminals = airport.getTerminals();
+        List<Flight> flights = new ArrayList<>();
+
+        for (Terminal value : terminals)
+            flights.addAll(value.getFlights());
+
+        return
+                flights.stream().filter((f) -> {
+                    long flightsTime = f.getDate().getTime();
+                    long currentTime = new Date().getTime();
+                    long twoHours = 1000 * 60 * 60 * 2;
+                    boolean timeMatched = false;
+                    long time = flightsTime - currentTime;
+                    if (time >= 0 && time <= twoHours)
+                        timeMatched = true;
+                    Flight.Type type = f.getType();
+                    boolean typeMatched = (type == Flight.Type.DEPARTURE);
+                    return typeMatched && timeMatched;
+                }).collect(Collectors.toList());
+
     }
 
 }
