ac6c416ac6c7e333356dd62c840978fe42077fe2
Виталий Белоцерковский <vitaly.belo@gmail.com>
2022-09-06 13:10:39 +0000

    Update Main.java

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -4,6 +4,8 @@ import com.skillbox.airport.Flight;
 import com.skillbox.airport.Terminal;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -17,25 +19,56 @@ public class Main {
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
         //TODO Метод должен вернуть список рейсов вылетающих в ближайшие два часа.
-        List<Terminal> terminals = airport.getTerminals();
-        List<Flight> flights = new ArrayList<>();
 
-        for (Terminal value : terminals)
-            flights.addAll(value.getFlights());
-
-        return
-                flights.stream().filter((f) -> {
-                    long flightsTime = f.getDate().getTime();
-                    long currentTime = new Date().getTime();
-                    long twoHours = 1000 * 60 * 60 * 2;
-                    boolean timeMatched = false;
-                    long time = flightsTime - currentTime;
-                    if (time >= 0 && time <= twoHours)
-                        timeMatched = true;
-                    boolean typeMatched = (f.getType() == Flight.Type.DEPARTURE);
-                    return typeMatched && timeMatched;
+        return airport.getTerminals().stream().flatMap(t -> t.getFlights().stream())
+                .filter(f -> f.getType().equals(Flight.Type.DEPARTURE))
+                .filter(f -> {
+                        LocalDateTime nowTime = LocalDateTime.now();
+                        LocalDateTime flyTime =
+                            LocalDateTime.ofInstant(f.getDate().toInstant(), ZoneId.systemDefault());
+                        return flyTime.isAfter(nowTime) && flyTime.isBefore(nowTime.plusHours(2));
                 }).collect(Collectors.toList());
-
     }
 
+//    public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
+//        //TODO Метод должен вернуть список рейсов вылетающих в ближайшие два часа.
+//        List<Terminal> terminals = airport.getTerminals();
+//        List<Flight> flights = new ArrayList<>();
+//
+//        for (Terminal value : terminals)
+//            flights.addAll(value.getFlights());
+//
+//        return
+//                flights.stream()
+//                .filter(f -> f.getType().equals(Flight.Type.DEPARTURE))
+//                .filter(f -> {
+//                    LocalDateTime nowTime = LocalDateTime.now();
+//                    LocalDateTime flyTime =
+//                            LocalDateTime.ofInstant(f.getDate().toInstant(), ZoneId.systemDefault());
+//                    return flyTime.isAfter(nowTime) && flyTime.isBefore(nowTime.plusHours(2));
+//                }).collect(Collectors.toList());
+//    }
+
+//    public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
+//        //TODO Метод должен вернуть список рейсов вылетающих в ближайшие два часа.
+//        List<Terminal> terminals = airport.getTerminals();
+//        List<Flight> flights = new ArrayList<>();
+//
+//        for (Terminal value : terminals)
+//            flights.addAll(value.getFlights());
+//
+//        return
+//                flights.stream().filter((f) -> {
+//                    long flightsTime = f.getDate().getTime();
+//                    long currentTime = new Date().getTime();
+//                    long twoHours = 1000 * 60 * 60 * 2;
+//                    boolean timeMatched = false;
+//                    long time = flightsTime - currentTime;
+//                    if (time >= 0 && time <= twoHours)
+//                        timeMatched = true;
+//                    boolean typeMatched = (f.getType() == Flight.Type.DEPARTURE);
+//                    return typeMatched && timeMatched;
+//                }).collect(Collectors.toList());
+//    }
+    
 }
