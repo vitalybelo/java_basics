47345f531cb04f1e8e346b3e7cfffe2f5875c168
Виталий Белоцерковский <vitaly.belo@gmail.com>
2022-09-07 06:56:45 +0000

    Update Main.java

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -1,11 +1,12 @@
-import com.skillbox.airport.Aircraft;
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
 import com.skillbox.airport.Terminal;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -19,16 +20,33 @@ public class Main {
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
         //TODO Метод должен вернуть список рейсов вылетающих в ближайшие два часа.
+        List<Flight> flights = new ArrayList<>();
+        for (Terminal value : airport.getTerminals())
+                flights.addAll(value.getFlights());
 
-        return airport.getTerminals().stream().flatMap(t -> t.getFlights().stream())
-                .filter(f -> f.getType().equals(Flight.Type.DEPARTURE))
-                .filter(f -> {
-                        LocalDateTime nowTime = LocalDateTime.now();
-                        LocalDateTime flyTime =
-                            LocalDateTime.ofInstant(f.getDate().toInstant(), ZoneId.systemDefault());
-                        return flyTime.isAfter(nowTime) && flyTime.isBefore(nowTime.plusHours(2));
-                }).collect(Collectors.toList());
+        Date dateNow = Date.from(Instant.now());
+        Date dateP2h = Date.from(Instant.now().plus(2, ChronoUnit.HOURS));
+        return
+                flights.stream()
+                        .filter( f -> f.getType() == Flight.Type.DEPARTURE)
+                        .filter( f -> f.getDate().after(dateNow))
+                        .filter( f -> f.getDate().before(dateP2h))
+                .collect(Collectors.toList());
     }
+
+
+//    public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
+//        //TODO Метод должен вернуть список рейсов вылетающих в ближайшие два часа.
+//
+//        return airport.getTerminals().stream().flatMap(t -> t.getFlights().stream())
+//                .filter(f -> f.getType().equals(Flight.Type.DEPARTURE))
+//                .filter(f -> {
+//                        LocalDateTime nowTime = LocalDateTime.now();
+//                        LocalDateTime flyTime =
+//                            LocalDateTime.ofInstant(f.getDate().toInstant(), ZoneId.systemDefault());
+//                        return flyTime.isAfter(nowTime) && flyTime.isBefore(nowTime.plusHours(2));
+//                }).collect(Collectors.toList());
+//    }
 
 //    public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
 //        //TODO Метод должен вернуть список рейсов вылетающих в ближайшие два часа.
@@ -49,26 +67,5 @@ public class Main {
 //                }).collect(Collectors.toList());
 //    }
 
-//    public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-//        //TODO Метод должен вернуть список рейсов вылетающих в ближайшие два часа.
-//        List<Terminal> terminals = airport.getTerminals();
-//        List<Flight> flights = new ArrayList<>();
-//
-//        for (Terminal value : terminals)
-//            flights.addAll(value.getFlights());
-//
-//        return
-//                flights.stream().filter((f) -> {
-//                    long flightsTime = f.getDate().getTime();
-//                    long currentTime = new Date().getTime();
-//                    long twoHours = 1000 * 60 * 60 * 2;
-//                    boolean timeMatched = false;
-//                    long time = flightsTime - currentTime;
-//                    if (time >= 0 && time <= twoHours)
-//                        timeMatched = true;
-//                    boolean typeMatched = (f.getType() == Flight.Type.DEPARTURE);
-//                    return typeMatched && timeMatched;
-//                }).collect(Collectors.toList());
-//    }
-    
+
 }
