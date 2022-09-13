c5551b2ffdc2d92e9893c511634fa664ba7e3aa9
vitaly beloserkovskii <vitaly.belo@gmail.com>
2022-09-13 16:00:58 +0300

    final

--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -14,16 +14,38 @@ public class CustomerStorage {
         final int INDEX_EMAIL = 2;
         final int INDEX_PHONE = 3;
 
+        String regexPhone = "^((8|\\+7)[\\- ]?)?(\\(?\\d{3}\\)?[\\- ]?)?[\\d\\- ]{7,10}$";
+        String regexEmail = "^([\\w-\\.]+){1,64}@([\\w&&[^_]]+){2,255}.[a-z]{2,}$";
+
         String[] components = data.split("\\s+");
+        if (components.length != 4) {
+            Main.errorLoger.info("Неверный формат строки добавления: add " + data);
+            throw new ArrayIndexOutOfBoundsException("Неправильный формат строки. Правильно:\n" +
+                    "add Василий Петров vasily.petrov@gmail.com +79215637722");
+        }
         String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
+        if (!components[INDEX_PHONE].matches(regexPhone)) {
+            Main.errorLoger.info("Неверный формат номера телефона: " + components[INDEX_PHONE]);
+            throw new IllegalArgumentException ("Неверный формат номера телефона\n");
+        };
+        if (!components[INDEX_EMAIL].matches(regexEmail)) {
+            Main.errorLoger.info("Неверный формат электронной почты: " + components[INDEX_EMAIL]);
+            throw new IllegalArgumentException ("Неверный формат электронной почты\n");
+        }
         storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
+
     }
 
     public void listCustomers() {
-        storage.values().forEach(System.out::println);
+        if (storage.size() > 0)
+            storage.values().forEach(System.out::println);
     }
 
     public void removeCustomer(String name) {
+        if (!storage.containsKey(name)) {
+            Main.errorLoger.info("Удаление не выполнено, клиент " + name + " не найден.");
+            throw new IllegalArgumentException("Удаление не выполнено, клиент " + name + " не найден.");
+        }
         storage.remove(name);
     }
 
--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/Main.java
+++ ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/Main.java
@@ -1,6 +1,12 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.util.Scanner;
 
 public class Main {
+
+    public static Logger logger;
+    public static Logger errorLoger;
+    public static Logger queryLoger;
     private static final String ADD_COMMAND = "add Василий Петров " +
             "vasily.petrov@gmail.com +79215637722";
     private static final String COMMAND_EXAMPLES = "\t" + ADD_COMMAND + "\n" +
@@ -12,23 +18,44 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         CustomerStorage executor = new CustomerStorage();
+        logger = LogManager.getRootLogger();
+        errorLoger = LogManager.getLogger("ErrorLogger");
+        queryLoger = LogManager.getLogger("QueriesLogger");
 
         while (true) {
             String command = scanner.nextLine();
             String[] tokens = command.split("\\s+", 2);
 
-            if (tokens[0].equals("add")) {
-                executor.addCustomer(tokens[1]);
-            } else if (tokens[0].equals("list")) {
-                executor.listCustomers();
-            } else if (tokens[0].equals("remove")) {
-                executor.removeCustomer(tokens[1]);
-            } else if (tokens[0].equals("count")) {
-                System.out.println("There are " + executor.getCount() + " customers");
-            } else if (tokens[0].equals("help")) {
-                System.out.println(helpText);
-            } else {
-                System.out.println(COMMAND_ERROR);
+            queryLoger.info("Введена консольная команда: " + command);
+            try {
+                switch (tokens[0]) {
+                    case "add":
+                        executor.addCustomer(tokens[1]);
+                        break;
+                    case "list":
+                        executor.listCustomers();
+                        break;
+                    case "remove":
+                        if (tokens.length == 1) {
+                            errorLoger.info("Попытка удаления записи без параметра");
+                            throw new IllegalArgumentException
+                                    ("\nНе указано имя клиента. Правильно:\nremove Василий Петров.\n");
+                        }
+                        executor.removeCustomer(tokens[1]);
+                        break;
+                    case "count":
+                        System.out.println("There are " + executor.getCount() + " customers");
+                        break;
+                    case "help":
+                        System.out.println(helpText);
+                        break;
+                    default:
+                        System.out.println(COMMAND_ERROR);
+                        break;
+                }
+            } catch (IllegalArgumentException | ArrayIndexOutOfBoundsException ex) {
+                logger.info("Обработана ошибка: " + ex.getMessage());
+                ex.printStackTrace();
             }
         }
     }
