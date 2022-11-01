2c67ef78666603a15d5f446373a05f0412af8262
vitaly beloserkovskii <vitaly.belo@gmail.com>
2022-11-01 09:30:17 +0300

    init

--- SpringBootWebDevelopment/ToDoList/ToDoList/src/main/java/main/DefaultController.java
+++ SpringBootWebDevelopment/ToDoList/ToDoList/src/main/java/main/DefaultController.java
@@ -1,4 +1,19 @@
 package main;
 
+import com.fasterxml.jackson.databind.DatabindException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Date;
+
+@RestController
 public class DefaultController {
+
+    @RequestMapping("/")
+    public String index() {
+
+        System.out.println(Math.random() + "<br/>");
+        return (new Date()).toInstant().toString();
+
+    }
+
 }
