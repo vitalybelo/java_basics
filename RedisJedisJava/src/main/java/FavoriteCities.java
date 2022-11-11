c723b9da911a63c6d5a4b07c72bb8cbc7b521467
vitaly beloserkovskii <vitaly.belo@gmail.com>
2022-11-11 15:37:08 +0300

    finalize

--- RedisJedisJava/src/main/java/FavoriteCities.java
+++ RedisJedisJava/src/main/java/FavoriteCities.java
@@ -1,2 +1,10 @@
+import redis.clients.jedis.*;
+
 public class FavoriteCities {
+
+    private Jedis client = new Jedis();
+
+    public FavoriteCities(Jedis client) {
+        this.client = client;
+    }
 }
--- RedisJedisJava/src/main/java/Main.java
+++ RedisJedisJava/src/main/java/Main.java
@@ -1,9 +1,34 @@
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        // Create a Jedis connection pool
+        JedisPool jedisPool = new JedisPool(new JedisPoolConfig(), "localhost", 6379);
 
+        // Get the pool and use the database
+        try (Jedis jedis = jedisPool.getResource()) {
+
+            jedis.set("mykey", "Hello from Jedis");
+            String value = jedis.get("mykey");
+            System.out.println( value );
+
+            jedis.zadd("vehicles", 0, "car");
+            jedis.zadd("vehicles", 0, "bike");
+            List<String> vehicles = jedis.zrange("vehicles", 0, -1);
+            System.out.println( vehicles );
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // close the connection pool
+        jedisPool.close();
 
     }
 }
