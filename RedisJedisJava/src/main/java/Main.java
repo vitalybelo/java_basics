0801b906ac250214117ba84dc405359303d2c536
vitaly beloserkovskii <vitaly.belo@gmail.com>
2022-11-12 10:01:36 +0300

    finalize

--- RedisJedisJava/src/main/java/Main.java
+++ RedisJedisJava/src/main/java/Main.java
@@ -2,17 +2,14 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
 
-
         // Инициализация сервера (должен быть запущен сервер в powerPoint > redis-server)
         JedisPool jedisPool = new JedisPool(new JedisPoolConfig(), "localhost", 6379);
 
-        // Get the pool and use the database
+        // Get the pool and use the database (great thanks to Baeldung education platform !)
         try (Jedis jedis = jedisPool.getResource()) {
 
             System.out.println("\nЗадание №1");
