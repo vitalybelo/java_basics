6f9560b26c7f0b694b613a9b7b72bf4a328fe536
vitaly beloserkovskii <vitaly.belo@gmail.com>
2022-11-12 17:39:34 +0300

    init

--- DesktopApp/src/DialogForm.java
+++ DesktopApp/src/DialogForm.java
@@ -9,6 +9,11 @@ public class DialogForm extends JDialog {
     private JTextField textField1;
     private JTextField textField2;
     private JTextField textField3;
+    private JTextField textField4;
+    private JLabel label4;
+    private JLabel label1;
+    private JLabel label2;
+    private JLabel label3;
 
     public DialogForm() {
 
@@ -45,16 +50,16 @@ public class DialogForm extends JDialog {
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
     }
 
-    private void onOK() {
-        // add your code here
+    private boolean collapsePuzzle() {
+
         if (textField1.getText().length() == 0) {
             JOptionPane.showMessageDialog(
                     contentPanel,
                     "Фамилия не указана. Это обязательно поле.",
                     "Предупреждение",
                     JOptionPane.ERROR_MESSAGE
-            );
-            return;
+                    );
+            return false;
         }
         if (textField2.getText().length() == 0) {
             JOptionPane.showMessageDialog(
@@ -62,14 +67,75 @@ public class DialogForm extends JDialog {
                     "Имя не указано. Это обязательно поле.",
                     "Предупреждение",
                     JOptionPane.ERROR_MESSAGE
-            );
-            return;
+                    );
+            return false;
         }
-        String button_text = button_ok.getText();
-        if (button_text.equals("Collapse")) {
-            button_ok.setText("Expand");
+        // фамилия и имя введены верно
+        return true;
+    }
+
+    private boolean expandPuzzle() {
+
+        String nameText = textField4.getText();
+        String[] words = nameText.split(" ");
+        if (words.length < 2) {
+            JOptionPane.showMessageDialog(
+                    contentPanel,
+                    "Фамилия и имя должны быть указаны.",
+                    "Предупреждение",
+                    JOptionPane.ERROR_MESSAGE
+                    );
+            return false;
         } else {
-            button_ok.setText("Collapse");
+            textField1.setText(words[0]);
+            textField2.setText(words[1]);
+            if (words.length >= 3)
+                textField3.setText(words[2]);
+        }
+        return true;
+    }
+
+    private void onOK() {
+
+        String button_text = button_ok.getText();
+
+        if (button_text.equals("Collapse")) {
+
+            if (collapsePuzzle()) {
+                // переход с Collapse на Expand
+                label4.setVisible(true);
+                textField4.setVisible(true);
+
+                label1.setVisible(false);
+                label2.setVisible(false);
+                label3.setVisible(false);
+                textField1.setVisible(false);
+                textField2.setVisible(false);
+                textField3.setVisible(false);
+
+                textField4.setText(textField1.getText() + " " + textField2.getText() + " " + textField3.getText());
+                textField1.setText("");
+                textField2.setText("");
+                textField3.setText("");
+                button_ok.setText("Expand");
+            }
+        } else {
+
+            if (expandPuzzle()) {
+                // переход с Expand на Collapse
+                label4.setVisible(false);
+                textField4.setVisible(false);
+
+                label1.setVisible(true);
+                label2.setVisible(true);
+                label3.setVisible(true);
+                textField1.setVisible(true);
+                textField2.setVisible(true);
+                textField3.setVisible(true);
+
+                textField4.setText("");
+                button_ok.setText("Collapse");
+            }
         }
 
     }
