530c48a8766074e693a64918fa5061dad169b2b5
vitaly beloserkovskii <vitaly.belo@gmail.com>
2022-11-12 09:59:17 +0300

    finalize

--- RedisJedisJava/src/main/java/FavoriteCities.java
+++ RedisJedisJava/src/main/java/FavoriteCities.java
@@ -1,10 +1,36 @@
 import redis.clients.jedis.*;
 
+import java.util.List;
+
 public class FavoriteCities {
 
-    private Jedis client = new Jedis();
+    private final Jedis jedis;
 
     public FavoriteCities(Jedis client) {
-        this.client = client;
+        this.jedis = client;
     }
+
+    public void listCities() {
+
+        jedis.zadd("Favorite Cities",69000, "Bali");
+        jedis.zadd("Favorite Cities",89000, "Singapore");
+        jedis.zadd("Favorite Cities",29000, "Paris");
+        jedis.zadd("Favorite Cities",34000, "London");
+        jedis.zadd("Favorite Cities",65000, "Washington");
+        jedis.zadd("Favorite Cities",23000, "Rome");
+        jedis.zadd("Favorite Cities",25000, "Neapol");
+        jedis.zadd("Favorite Cities",31000, "Venice");
+        jedis.zadd("Favorite Cities",15000, "Berlin");
+        jedis.zadd("Favorite Cities",45000, "Madrid");
+
+
+        System.out.print("ТОП 3 самых дешёвых путешествий: ");
+        System.out.println(jedis.zrange("Favorite Cities",0,2));
+
+        System.out.print("ТОП 3 самых дорогих путешествий: ");
+        System.out.println(jedis.zrevrange("Favorite Cities",0,2));
+
+        jedis.del("Favorite Cities");
+    }
+
 }
--- RedisJedisJava/src/main/java/Main.java
+++ RedisJedisJava/src/main/java/Main.java
@@ -8,27 +8,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Create a Jedis connection pool
+
+        // Инициализация сервера (должен быть запущен сервер в powerPoint > redis-server)
         JedisPool jedisPool = new JedisPool(new JedisPoolConfig(), "localhost", 6379);
 
         // Get the pool and use the database
         try (Jedis jedis = jedisPool.getResource()) {
 
-            jedis.set("mykey", "Hello from Jedis");
-            String value = jedis.get("mykey");
-            System.out.println( value );
+            System.out.println("\nЗадание №1");
+            FavoriteCities cities = new FavoriteCities(jedis);
+            cities.listCities();
 
-            jedis.zadd("vehicles", 0, "car");
-            jedis.zadd("vehicles", 0, "bike");
-            List<String> vehicles = jedis.zrange("vehicles", 0, -1);
-            System.out.println( vehicles );
+
 
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        // close the connection pool
         jedisPool.close();
-
     }
 }
