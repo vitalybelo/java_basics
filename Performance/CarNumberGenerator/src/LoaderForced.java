0e874cd0ea2f88266be8885a5b95da7f5ace7b8d
vitaly beloserkovskii <vitaly.belo@gmail.com>
2022-11-16 16:06:23 +0300

    init

---
+++ Performance/CarNumberGenerator/src/LoaderForced.java
@@ -0,0 +1,44 @@
+import java.io.FileOutputStream;
+
+public class LoaderForced {
+
+    public static void main(String[] args) throws Exception {
+
+        long ms1 = System.currentTimeMillis();
+
+        FileOutputStream writer = new FileOutputStream("res/numbers.txt");
+
+        char[] letters = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
+
+        for (int number = 1; number < 1000; number++) {
+            int regionCode = 199;
+            for (char firstLetter : letters) {
+                for (char secondLetter : letters) {
+                    for (char thirdLetter : letters) {
+                        String carNumber = firstLetter + padNumber(number, 3) +
+                                secondLetter + thirdLetter + padNumber(regionCode, 2);
+                        writer.write(carNumber.getBytes());
+                        writer.write('\n');
+                    }
+                }
+            }
+        }
+
+        writer.flush();
+        writer.close();
+
+        System.out.println((System.currentTimeMillis() - ms1) + " ms");
+    }
+
+    private static String padNumber(int number, int numberLength) {
+        String numberStr = Integer.toString(number);
+        int padSize = numberLength - numberStr.length();
+
+        for (int i = 0; i < padSize; i++) {
+            numberStr = '0' + numberStr;
+        }
+
+        return numberStr;
+    }
+
+}
